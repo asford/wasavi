e38a44ee8e70b0ffbaa7387f8a3707cd7f1767f4
Alex Ford <fordas@uw.edu>
2018-05-06 09:41:39 -0700

    Fixup marked paragraph rendering.
    Remove custom paragraph/header rendering in html conversion to fixup
    line break and line spacing behavior in inbox/gmail.

--- src/chrome/backend/lib/main.js
+++ src/chrome/backend/lib/main.js
@@ -667,8 +667,6 @@
 		&&  'writeAs' in data.payload
 		&&  data.payload.writeAs == 'html') {
 			var renderer = new marked.Renderer;
-			renderer.heading = rendererHeading;
-			renderer.paragraph = rendererParagraph;
 			renderer.strong = rendererStrong;
 			renderer.em = rendererEm;
 
